da0d98d519b226ee31b134bea703cda670f3a81c
Mike Wallick <mike@wallick.net>
2025-04-07 08:46:50 -0500

    rebranded for basic acul demo

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -42,13 +42,23 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 export const GET = handleAuth({
   login: handleLogin(req => {
     const { searchParams } = new URL(req.url);
+    const screen_hint = searchParams.get('screen_hint') || undefined;
+    const connection = searchParams.get('connection') || undefined;
+    const login_hint = searchParams.get('login_hint') || undefined;
+    const returnTo = searchParams.get('returnTo') || '/';
+
+    console.log('screen_hint', screen_hint);
+    console.log('connection', connection);
+    console.log('login_hint', login_hint);
+    console.log('return_to', returnTo);
+
     return {
       authorizationParams: {
-        screen_hint: searchParams.get('screen_hint') || undefined,
-        connection: searchParams.get('connection') || undefined,
-        login_hint: searchParams.get('login_hint') || undefined
+        screen_hint: screen_hint,
+        connection: connection,
+        login_hint: login_hint
       },
-      returnTo: searchParams.get('returnTo') || '/'
+      returnTo: returnTo
     };
   })
 });
